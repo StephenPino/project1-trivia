146621355fab16dbd4ce95735c6a8c73fe7b35a0
Stephen Pino <spino725@gmail.com>
2017-03-11 13:57:34 -0600

    changed some CSS elements

--- assets/javascript/interface.js
+++ assets/javascript/interface.js
@@ -1,3 +1,22 @@
+// Update this in code
+function hideNameDisplay(num){
+	$("#player" + num + "-name-input").addClass("hidden");
+	$("#player" + num + "-name-submit").addClass("hidden");
+	$("#player" + num + "-points-p").removeClass("hidden");
+}
+
+function showNameDisplay(num){
+	$("#player" + num + "-name-input").removeClass("hidden");
+	$("#player" + num + "-name-submit").removeClass("hidden");
+	$("#player" + num + "-points-p").addClass("hidden");
+}
+
+$("#player2-name-submit").on("click", function() {
+hideNameDisplay(2);
+
+});
+
+
 // Ready / Unready Status
 $("#player-ready").on("click", function() {
 
